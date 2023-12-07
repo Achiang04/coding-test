3a2ce38b433e0175e7d956b73e39837affa35d23
jecky hindrawan <jeckyhindrawan123@gmail.com>
2023-12-08 01:40:07 +0700

    -

--- syukur-gabung/database.sql
+++ syukur-gabung/database.sql
@@ -77,3 +77,12 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 )
+
+CREATE TABLE `crud` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+)
